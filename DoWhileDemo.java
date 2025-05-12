f911579e6f189e6b8a696769761e2c5d63aca627
Hanif Mohammad <70031945+mdhanifhashmi@users.noreply.github.com>
2025-05-12 23:37:29 +0530

    Create DoWhileDemo.java

---
+++ DoWhileDemo.java
@@ -0,0 +1,9 @@
+class DoWhileDemo{
+    public static void main( String[] args){
+        int a=1;
+        do{
+            System.out.println(a);
+            a = a + 1;
+        }while(a<=5);
+    }
+}
