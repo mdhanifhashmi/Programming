8ed10d9b18811d59a288f736a19abadfab5b25bb
Hanif Mohammad <70031945+mdhanifhashmi@users.noreply.github.com>
2025-05-12 23:36:09 +0530

    Create ArrayDemo.java

---
+++ ArrayDemo.java
@@ -0,0 +1,22 @@
+class ArrayDemo{
+    public static void main( String[] args){
+        byte[] b1 = new byte[5] ;
+        short[] s1 = new short[5];
+        int[] i1 = new int[5];
+        long[] l1 = new long[5];
+        float[] f1 = new float[5];
+        double[] d1 = new double[5];
+        String[] S1 = new String[5];
+        char[] c1 = new char[5];
+        System.out.println("data@addesss");
+        System.out.println(b1);
+        System.out.println(s1);
+        System.out.println(i1);
+        System.out.println(l1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(S1);
+        System.out.println(c1);
+        System.out.println(c1.toString());
+    }
+}
