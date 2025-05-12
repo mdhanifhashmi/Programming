9d3023f4eb860b9439d8d650d2d78b1b0e13bbe8
Hanif Mohammad <70031945+mdhanifhashmi@users.noreply.github.com>
2025-05-12 23:38:04 +0530

    Create WhileDemo.java

---
+++ WhileDemo.java
@@ -0,0 +1,10 @@
+class WhileDemo{
+    public static void main(String[] args   ){
+        // intializatio
+        int a=1;
+        while( a <= 5){ //condition
+            System.out.println(a);
+            a=a+1; //updators
+        }
+    }
+}
