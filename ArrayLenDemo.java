c200606a6411d5c5468a3bcedec5f86fca4bccd7
Hanif Mohammad <70031945+mdhanifhashmi@users.noreply.github.com>
2025-05-12 23:36:57 +0530

    Create ArrayLenDemo.java

---
+++ ArrayLenDemo.java
@@ -0,0 +1,13 @@
+class ArrayLenDemo{
+    public static void main( String[] args){
+
+        String[] chetan = {"priya", "pooja","andhiya"};
+        System.out.println(chetan.length);
+        System.out.println(chetan[0]);
+        System.out.println(chetan[1]);
+        System.out.println(chetan[2]);
+        // System.out.println(chetan[ ]);
+
+
+    }
+}
