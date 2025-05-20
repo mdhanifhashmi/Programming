640f01a97100eed1d83231b5dba382a2ef4b41e3
Hanif Mohammad <70031945+mdhanifhashmi@users.noreply.github.com>
2025-05-20 23:55:13 +0530

    Create Hotel.java

---
+++ Encapsulation/Hotel.java
@@ -0,0 +1,26 @@
+package javabean;
+
+public class Hotel {
+	
+	private String name;
+	private int ratings;
+	
+	//setter method
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setRatings(int ratings) {
+		this.ratings = ratings;
+	}
+	
+	//getter method
+	public String getName() {
+		return name;
+	}
+	
+	public int getRatings() {
+		return ratings;
+	}
+
+}
