1eec32e9970c132ef05cef9b452739296f6f4f57
Hanif Mohammad <70031945+mdhanifhashmi@users.noreply.github.com>
2025-05-20 23:52:49 +0530

    Create Car.java

---
+++ Encapsulation/Car.java
@@ -0,0 +1,15 @@
+package javabean;
+
+public class Car {
+	private int cost;
+	
+	//setter method
+	public void setCost(int cost) {
+		this.cost= cost;	}
+	
+	//getter method
+	public int getCost() {
+		return cost;
+	}
+	
+}
